e19a6fdcfa67e4dc79746adb630e1c2dac94f1fe
Lê Hoài Nghĩa Hiệp <107872812+HoaiNghiaHiepLe@users.noreply.github.com>
2023-08-01 00:02:55 +0700

    Feature/product add to cart (#20)
    * reconstruct product component files
    
    * product detail UI & DOMpurify
    
    * product image slider
    
    * logic zoom image on mouse move
    
    * product detail url with slug
    
    * purchase API declaration
    
    * add product to cart | show products in cart

--- src/useRouteElements.tsx
+++ src/useRouteElements.tsx
@@ -32,7 +32,6 @@ export default function useRouteElements() {
     },
     {
       path: path.productDetail,
-      index: true,
       element: (
         <MainLayout>
           <ProductDetail />
