fe2f86713bb46803c97fe081ac0cf77f59f01924
HoaiNghiaHiepLe <hiep.lhn@gmail.com>
2023-08-11 00:35:06 +0700

    profile page UI

--- src/pages/User/pages/Profile/Profile.tsx
+++ src/pages/User/pages/Profile/Profile.tsx
@@ -1,3 +1,75 @@
+import Input from 'src/components/Input'
+
 export default function Profile() {
-  return <div>Profile</div>
+  return (
+    <div className='rounded-sm bg-white px-2 pb-10 shadow md:px-7 md:pb-20'>
+      <div className='border-b border-b-gray-200 py-6'>
+        <h1 className='text-lg font-medium capitalize text-gray-900'>Hồ sơ của tôi</h1>
+        <div className='mt-1 text-sm text-gray-700'>Quản lý thông tin hồ sơ để bảo mật tài khoản</div>
+      </div>
+      <form className='mt-8 flex flex-col-reverse md:flex-row md:items-start'>
+        <div className='mt-6 flex-grow md:mt-0 md:pr-12'>
+          <div className='flex flex-col flex-wrap sm:flex-row'>
+            <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>Email</div>
+            <div className='sm:w-[80%] sm:pl-5'>
+              <div className='pt-3 text-gray-700'>Hi**********@Gmail.com</div>
+            </div>
+          </div>
+          <div className='mt-6 flex flex-col flex-wrap sm:flex-row'>
+            <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>Tên</div>
+            <div className='w-[80%] sm:pl-5'>
+              <Input classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm' />
+            </div>
+          </div>
+          <div className='mt-2 flex flex-col flex-wrap sm:flex-row'>
+            <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>Số điện thoại</div>
+            <div className='w-[80%] sm:pl-5'>
+              <Input classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm' />
+            </div>
+          </div>
+          <div className='mt-2 flex flex-col flex-wrap sm:flex-row'>
+            <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>Địa chỉ</div>
+            <div className='w-[80%] sm:pl-5'>
+              <Input classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm' />
+            </div>
+          </div>
+          <div className='mt-2 flex flex-col flex-wrap sm:flex-row'>
+            <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>ngày sinh</div>
+            <div className='w-[80%] sm:pl-5'>
+              <div className='flex justify-between'>
+                <select className='border- h-10 w-[32%] rounded-sm border border-black/10 px-3 outline-none'>
+                  <option disabled>Ngày</option>
+                </select>
+                <select className='border- h-10 w-[32%] rounded-sm border border-black/10 px-3 outline-none'>
+                  <option disabled>Tháng</option>
+                </select>
+                <select className='border- h-10 w-[32%] rounded-sm border border-black/10 px-3 outline-none'>
+                  <option disabled>Năm</option>
+                </select>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='flex justify-center md:w-72 md:border-l md:border-l-gray-200'>
+          <div className='flex flex-col items-center'>
+            <div className='my-5 h-24 w-24'>
+              <img
+                className='h-full w-full rounded-full object-cover'
+                src='https://images.unsplash.com/photo-1689152842048-6f0e4ef8e3f6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80'
+                alt=''
+              />
+            </div>
+            <input className='hidden' type='file' accept='.jpg,.jpeg,.png' />
+            <button className='flex h-10 items-center justify-end rounded-sm border bg-white px-6 text-sm text-gray-600 shadow-sm'>
+              Chọn ảnh
+            </button>
+            <div className='mt-3 text-gray-400'>
+              <div>Dung lượng tối đa là 1 MB</div>
+              <div>Định dạng jpeg , png</div>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  )
 }
