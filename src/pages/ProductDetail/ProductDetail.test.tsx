83856d32ce8846e1e56e9af1a43b8518bd2664ca
HoaiNghiaHiepLe <hiep.lhn@gmail.com>
2023-08-25 10:51:30 +0700

    test snapShot technique

---
+++ src/pages/ProductDetail/ProductDetail.test.tsx
@@ -0,0 +1,14 @@
+import { screen } from '@testing-library/react'
+import { delay, renderWithRouter } from 'src/utils/testUtils'
+import { describe, expect, test } from 'vitest'
+
+describe('ProductDetail', () => {
+  test('Render UI ProductDetail', async () => {
+    renderWithRouter({
+      route: '/Điện-Thoại-Vsmart-Active-3-6GB64GB--Hàng-Chính-Hãng-i-60afb2c76ef5b902180aacba'
+    })
+    await delay(1000)
+    expect(document.body).toMatchSnapshot()
+    // await screen.debug(document.body.querySelector('.bg-gray-200') as HTMLElement)
+  })
+})
--- src/utils/testUtils.tsx
+++ src/utils/testUtils.tsx
@@ -6,22 +6,23 @@ import App from 'src/App'
 import { AppProvider, getInitialAppContext } from 'src/contexts/app.context'
 import { expect } from 'vitest'
 
-const delay = (time: number) => new Promise((resolve) => setTimeout(() => resolve(true), time))
+export const delay = (time: number) => new Promise((resolve) => setTimeout(() => resolve(true), time))
 
 export const logScreen = async (
   body: HTMLElement = document.body.parentElement as HTMLElement,
   options?: waitForOptions
 ) => {
   const { timeout = 1000 } = options || {}
-  await waitFor(
-    async () => {
-      expect(await delay(timeout - 100)).toBe(true)
-    },
-    {
-      ...options,
-      timeout
-    }
-  )
+  await delay(timeout)
+  // await waitFor(
+  //   async () => {
+  //     expect(await delay(timeout - 100)).toBe(true)
+  //   },
+  //   {
+  //     ...options,
+  //     timeout
+  //   }
+  // )
   screen.debug(body, 999999999)
 }
 
