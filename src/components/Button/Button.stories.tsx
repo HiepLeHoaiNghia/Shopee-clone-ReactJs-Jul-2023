d67d6faa502356f0dfb8bbd2f300a6eb79714114
HoaiNghiaHiepLe <hiep.lhn@gmail.com>
2023-08-30 23:42:01 +0700

    storybook for components

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,49 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import Button from './Button'
+
+const meta = {
+  title: 'Components/Button',
+  component: Button,
+  argTypes: {
+    isLoading: {
+      defaultValue: false,
+      description: 'Loading state of button'
+    },
+    children: {
+      description: 'Content of button',
+      table: { type: { summary: 'React.ReactNode' }, defaultValue: { summary: '' } }
+    },
+    className: {
+      description: 'Classname of button for CSS',
+      table: { type: { summary: 'string' }, defaultValue: { summary: '' } }
+    }
+  },
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'fullscreen'
+  }
+} satisfies Meta<typeof Button>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {
+  args: {
+    children: 'Đăng nhập',
+    className:
+      'inline-flex items-center justify-center bg-red-500 px-2 py-4 text-center text-sm uppercase text-white hover:bg-red-600',
+    isLoading: true,
+    disabled: true
+  }
+}
+
+export const Secondary: Story = {
+  args: {
+    children: 'Đăng Ký',
+    className:
+      'flex w-full items-center justify-center bg-gray-500 px-2 py-4 text-center text-sm uppercase text-white hover:bg-gray-600',
+    isLoading: true,
+    disabled: true
+  }
+}
