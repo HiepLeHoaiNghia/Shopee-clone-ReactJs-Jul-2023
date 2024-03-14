cfaf470ede4d22d84503ea6995561c244ce0a036
HoaiNghiaHiepLe <hiep.lhn@gmail.com>
2024-03-15 00:18:39 +0700

    refactor: get baseUrl from .env

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,5 +1,7 @@
+const { VITE_API_URL } = import.meta.env
+
 const config = {
-  baseUrl: 'https://api-ecom.duthanhduoc.com/',
+  baseUrl: VITE_API_URL,
   maxSizeUploadAvatar: 1048576 // bytes
 }
 export default config
